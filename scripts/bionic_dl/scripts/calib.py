e9d280dc9208bb279ce1b53dfd821e79b4274242
Haiik <11510135@mails.sustech.edu.cn>
2019-06-14 22:52:11 +0800

    scripts updated

--- scripts/bionic_dl/scripts/calib.py
+++ scripts/bionic_dl/scripts/calib.py
@@ -1,59 +1,93 @@
 # Scripts for apply calibration lively
-import calib_toolbox.calibration as calib
-from calib_toolbox.calibration.calib_main import make_calibrator
+from calib_toolbox.robot_controller.Franka_ROS import Franka_ROS_Controller
 from calib_toolbox.config import cfg
-import calib_toolbox.utils as utils
-import calib_toolbox.utils.io_old as io_old
 from calib_toolbox.utils.point_pair import PointPair, PointPairList
-from calib_toolbox.utils import transform as trans
-from transforms3d.euler import quat2euler, euler2quat
-
 from calib_toolbox.path_generator.path_generator import PathGenerator
-from calib_toolbox.robot_controller.robot_controller_main import make_robot_controller
-from calib_toolbox.calibration.circle_fitting_main import make_circle_extractor
+from calib_toolbox.calibration.calib_main import RANSAC
+from calib_toolbox.calibration.circle_fitting_main import CircleFitting_V2
+from calib_toolbox.cam_controller.photoneo import Photoneo_Controller
+import rospy
 import argparse
 import os
 import datetime
+import time
 import numpy as np
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Calibration Configs")
-    parser.add_argument("--config-file", default="../configs/SVD.yaml", metavar="FILE", help="path to config file")
-    parser.add_argument("--input-dir", default="../data/data_new/test_data.json", metavar="FILE", help="path to data files")
-    parser.add_argument("--output-dir", default="../data/", help="folder to output data files")
-    parser.add_argument("--data-type", default="new", help="format of data")
+    parser.add_argument("--config-file", default="/home/bionicdl-Mega/repos/flange_handeye_calibration/scripts/bionic_dl/configs/online_calib.yaml", metavar="FILE", help="path to config file")
     args = parser.parse_args()
 
-    robot_controller = make_robot_controller(cfg)
-    circle_extractor = make_circle_extractor(cfg)
-    calib = make_calibrator(cfg)
-    #
+    cfg.merge_from_file(args.config_file)
+
+    rospy.init_node('move_group', anonymous=True)
+
+    robot_controller = Franka_ROS_Controller(cfg)
+    camera_controller = Photoneo_Controller(cfg)
     path_generator = PathGenerator(cfg, robot_controller.get_curr_pose())
-    # path_generator = PathGenerator(cfg, np.array([1, 2, 3, 4, 5, 6, 7]))
 
-    date_str = str(datetime.datetime.now()).split(" ")[0] + str(datetime.datetime.now()).split(" ")[1]
-    folder_path = os.path.join(args.output_dir, date_str)
-    print(folder_path)
-    os.system("mkdir %s"%(folder_path))
+    # Create folder
+    folder_name = time.strftime("%Y-%m-%d", time.localtime(int(round(time.time() * 1000)) / 1000))
+    folder_path = os.path.join(cfg.DATA_PATH, folder_name)
+    folder_path_final = folder_path
+    folder_idx = 0
 
-    data_path = os.path.join(folder_path, "data.json")
-    result_path = os.path.join(folder_path, "result.json")
+    while os.path.exists(folder_path_final):
+        folder_path_final = folder_path + "-" + str(folder_idx)
+        folder_idx += 1
+
+    if not os.path.exists(folder_path_final):
+        os.makedirs(folder_path_final)
+        print("Folder created: %s"%(folder_path_final))
+
+    folder_path = folder_path_final
+
+    data_path = os.path.join(folder_path, "calib_data.json")
+    result_path = os.path.join(folder_path, "calib_result.json")
     pp_list = PointPairList()
+    index = -1
 
+    # Start Calibration
+    path_generator.curr_idx = 8
     while True:
+
+        # Get next pose from path generator
         pose_next = path_generator.get_waypoint_next()
-        if len(pose_next) < 0:
+        if len(pose_next) == 0:
             break
+        if index >= len(path_generator.interp_path_list):
+            break
+        index += 1
 
+        # Move robot
         robot_controller.move(pose_next)
-        point_cloud_path = "../data/"
-        p_camera = circle_extractor(point_cloud_path)
-        p_robot = robot_controller.get_curr_pose()[:3]
-        # new_idx_list.append(pp_list.add_point(p_robot, p_camera))
-        pp_list.add_point(p_robot, p_camera)
 
-        # Apply Calibration when points are valid
-        if pp_list.get_len() > cfg.CALIBRATION.MIN_POINT:
-            pp_list.write_data(data_path)
-            H = calib(pp_list)
-            calib.write_result(result_path, H)
+        # Obtain piont cloud from camera controller
+        # FIXME: Unreasonable controlling command
+        point_cloud_dir = os.path.join(folder_path, "0 "+str(index))
+        camera_controller.get_image(point_cloud_dir)
+        point_cloud_dir = os.path.join(os.path.dirname(point_cloud_dir), "0"+str(index)+".ply")
+
+        if "Calib" in cfg.MODE:
+            # Circle extractor
+            circle_extractor = CircleFitting_V2(cfg)
+            p_camera = circle_extractor(point_cloud_dir, 0)
+        else:
+            p_camera = np.array([])
+
+        p_robot = robot_controller.get_curr_pose()
+        list_idx = pp_list.add_point(p_robot, p_camera)
+
+        # FIXME: Convert unit from m to mm for temporally use
+        pp_list.pp_dict[list_idx].add_field("robot_pose", p_robot*1000)
+
+        pp_list.pp_dict[list_idx].add_field("point_cloud_dir", point_cloud_dir)
+        pp_list.write_data(data_path)
+
+        if "Calib" in cfg.MODE:
+            pp_list.remove_empty()
+            if pp_list.get_len() > 4:
+                calib = RANSAC(cfg)
+                H = calib(pp_list)
+                pp_list.write_result(result_path, H)
+
--- scripts/bionic_dl/scripts/test.py
+++ scripts/bionic_dl/scripts/test.py
@@ -1,14 +1,10 @@
 # Script for calibration using existing data
-
-import calib_toolbox.calibration as calib
-from calib_toolbox.calibration.calib_main import make_calibrator
 from calib_toolbox.config import cfg
-import calib_toolbox.utils as utils
 import calib_toolbox.utils.io_old as io_old
 from calib_toolbox.utils.point_pair import PointPair, PointPairList
-from calib_toolbox.utils import transform as trans
-from transforms3d.euler import quat2euler, euler2quat
-# from calib_toolbox.calibration.calib_algorithms import save_result
+from calib_toolbox.calibration.calib_main import SVD, RANSAC
+from calib_toolbox.calibration.circle_fitting_main import CircleFitting_V2, CircleFitting_V1
+import os
 import argparse
 
 def read_old_data(data_path):
@@ -23,33 +19,29 @@ def read_old_data(data_path):
 
 def main():
     parser = argparse.ArgumentParser(description="Calibration Configs")
-    parser.add_argument("--config-file", default="../configs/SVD.yaml", metavar="FILE", help="path to config file")
-    parser.add_argument("--input-dir", default="../data/data_new/test_data.json", metavar="FILE", help="path to data files")
-    parser.add_argument("--output-dir", default="../data/data_new/test_result.json", help="folder to output data files")
+    parser.add_argument("--config-file", default="/home/bionicdl-Mega/repos/flange_handeye_calibration/scripts/bionic_dl/configs/offline_calib.yaml", metavar="FILE", help="path to config file")
     parser.add_argument("--data-type", default="new", help="format of data")
     args = parser.parse_args()
 
     cfg.merge_from_file(args.config_file)
 
+    data_path = cfg.DATA_PATH
+    result_path = os.path.join(os.path.dirname(data_path), "calib_result.json")
+
     pp_list = PointPairList()
-    print("Input Path:{}. Data type:{}".format(args.input_dir, args.data_type))
+    print("Input Path:{}. Data type:{}".format(data_path, args.data_type))
     if args.data_type == "old":
-        pp_list.read_old_data(args.input_dir)
+        pp_list.read_old_data(data_path)
     else:
-        pp_list.read(args.input_dir)
+        pp_list.read(data_path)
 
-    calib = make_calibrator(cfg)
+    calib = SVD(cfg)
+    circle_extractor = CircleFitting_V2(cfg)
+
+    # pp_list.circle_extraction(circle_extractor, overwrite=True, update_all=False)
+    pp_list.remove_empty()
+
     H = calib(pp_list)
-
-    # Codes for examine results
-    min_vec = trans.minvec_from_mat(H)
-    pose_vec = trans.vec_from_mat(H)
-    euler_org = min_vec[3:]
-    quat_org = pose_vec[3:]
-    euler = quat2euler(pose_vec[3:])
-    quat = euler2quat(euler_org[0], euler_org[1], euler_org[2])
-    H_min = trans.mat_from_minvec(min_vec)
-    H_norm = trans.mat_from_vec(pose_vec)
     pp_list.write_result(args.output_dir, H)
 
 
