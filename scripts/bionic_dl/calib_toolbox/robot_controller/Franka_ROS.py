82491593f66698add07be927a41adce8b8f061bd
Haiik <11510135@mails.sustech.edu.cn>
2019-06-14 22:53:36 +0800

    FRANKA controller updated

--- scripts/bionic_dl/calib_toolbox/robot_controller/Franka_ROS.py
+++ scripts/bionic_dl/calib_toolbox/robot_controller/Franka_ROS.py
@@ -17,7 +17,6 @@ from calib_toolbox.utils.transform_ros import pose_from_vector, pose_to_vector
 
 class Franka_ROS_Controller:
     def __init__(self, cfg):
-        # rospy.init_node()
         moveit_commander.roscpp_initialize(sys.argv)
         robot = moveit_commander.RobotCommander()
         scene = moveit_commander.PlanningSceneInterface()
@@ -27,7 +26,6 @@ class Franka_ROS_Controller:
         group.set_planning_time(5)
         group.set_max_velocity_scaling_factor(0.5)
         self.group = group
-
         tool = PoseStamped()
         tool.header.frame_id = "panda_link0"
         tool.pose.orientation.x = 0
@@ -37,6 +35,10 @@ class Franka_ROS_Controller:
         self.tool = tool
 
     def move(self, pose_vec):
+        """
+        Move robot to required pose
+        :param pose_vec: [x, y, z, q_w, q_x, q_y, q_z]
+        """
         group = self.group
         tool  = self.tool
 
@@ -44,20 +46,27 @@ class Franka_ROS_Controller:
         group.clear_pose_targets()
         tool.pose = pose_from_vector(pose_vec)
         group.set_pose_target(tool, end_effector_link='panda_link8')
-        plan = group.plan()
-        time.sleep(1)
-        res = group.execute(plan)
-        time.sleep(2)
-        if not res:
-            # if fail, swich robot back to mode 2 so that next move can be executed
-            # https://github.com/frankaemika/franka_ros/issues/69
-            os.system(
-                "rostopic pub -1 /franka_control/error_recovery/goal franka_control/ErrorRecoveryActionGoal \"{}\"")
+
+        while True:
+            plan = group.plan()
+            time.sleep(3)
+            res = group.execute(plan)
+            time.sleep(3)
+            if not res:
+                # if fail, swich robot back to mode 2 so that next move can be executed
+                # https://github.com/frankaemika/franka_ros/issues/69
+                os.system(
+                    "rostopic pub -1 /franka_control/error_recovery/goal franka_control/ErrorRecoveryActionGoal \"{}\"")
+                time.sleep(2)
+            else:
+                break
 
     def get_curr_pose(self):
+        """
+        Get current pose of robot
+        :return: pose_vec: [x, y, z, q_w, q_x, q_y, q_z]
+        """
+
         group  = self.group
         pose_ros = self.group.get_current_pose().pose
         return pose_to_vector(pose_ros)
-
-    def get_joint_pose(self):
-        pass
--- scripts/bionic_dl/calib_toolbox/robot_controller/robot_controller_main.py
+++ scripts/bionic_dl/calib_toolbox/robot_controller/robot_controller_main.py
@@ -1,8 +1,11 @@
 from calib_toolbox.robot_controller.Aubo_i5_ROS import Aubo_i5_ROS_controller
 from calib_toolbox.robot_controller.UR5_Socket import UR5_Controller_Socket
+from calib_toolbox.robot_controller.Franka_ROS import Franka_ROS_Controller
 
 def make_robot_controller(cfg):
     if cfg.ROBOT_CONTROLLER.ROBOT_TYPE == "Aubo_i5_ROS":
         return Aubo_i5_ROS_controller(cfg)
     elif cfg.ROBOT_CONTROLLER.ROBOT_TYPE == "UR5_SOCKET":
         return UR5_Controller_Socket(cfg)
+    elif cfg.ROBOT_CONTROLLER.ROBOT_TYPE == "FRANKA_ROS":
+        return Franka_ROS_Controller(cfg)
